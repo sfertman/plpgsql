498a05a4eabb893b5a45d81e9821d648233d5581
sfertman <sfertman@zoho.com>
2021-05-21 23:15:05 -0400

    Create jsonb_hash_set.sql

---
+++ jsonb_hash_set.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION fn_jbset(_ks text[])
+  RETURNS jsonb
+  LANGUAGE sql AS
+$BODY$
+  SELECT jsonb_object_agg(t, TRUE)
+  FROM unnest(_ks) t;
+$BODY$
+
+CREATE OR REPLACE FUNCTION fn_jbset_conj(_set jsonb, _ks text[])
+  RETURNS jsonb
+  LANGUAGE sql AS
+$BODY$
+  SELECT _set||jsonb_object_agg(t, TRUE)
+  FROM unnest(_ks) t;
+$BODY$
