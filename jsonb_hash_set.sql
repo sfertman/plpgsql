b90270fcea50afe1bdd750ac8f9e73ed0c779a15
sfertman <sfertman@zoho.com>
2021-05-21 23:17:03 -0400

    disj

--- jsonb_hash_set.sql
+++ jsonb_hash_set.sql
@@ -13,3 +13,11 @@ $BODY$
   SELECT _set||jsonb_object_agg(t, TRUE)
   FROM unnest(_ks) t;
 $BODY$
+
+CREATE OR REPLACE FUNCTION fn_jbset_disj(_set jsonb, _ks text[])
+  RETURNS jsonb
+  LANGUAGE sql AS
+$BODY$
+  SELECT _set-_ks;
+$BODY$
+
